c0eca6868ce9059a0108f54a054b7174f2fc2fd9
Konstantin Front <frontkot@gmail.com>
2021-06-19 20:16:24 +0300

    add final edits to proj

--- src/App.js
+++ src/App.js
@@ -2,26 +2,24 @@ import './App.scss';
 import GameColumn from './components/GameColumn/GameColumn';
 import StartButton from './components/StartButton/StartButton';
 import { useState } from 'react';
-import { gameArr, startGame, columnAction, randomNumber } from './utils';
+import { interval, gameArr, startGame, columnAction, randomNumber } from './utils';
 
 const App = () => {
-  const [firstColumnArr, setFirstColumnArr] = useState(gameArr.reverse());
-  const [secondColumnArr, setSecondColumnArr] = useState(gameArr.reverse());
-  const [thirdColumnArr, setThirdColumnArr] = useState(gameArr.reverse());
-  const [fourthColumnArr, setFourthColumnArr] = useState(gameArr.reverse());
-  const [fifthColumnArr, setFifthColumnArr] = useState(gameArr.reverse());
+  const [firstColumnArr, setFirstColumnArr] = useState(gameArr);
+  const [secondColumnArr, setSecondColumnArr] = useState(gameArr);
+  const [thirdColumnArr, setThirdColumnArr] = useState(gameArr);
+  const [fourthColumnArr, setFourthColumnArr] = useState(gameArr);
+  const [fifthColumnArr, setFifthColumnArr] = useState(gameArr);
 
-  const interval = 150;
-  const timerPlus = 5;
-  const setArr = [
-    {arr: firstColumnArr, numOfActions: randomNumber(5, 10), timeoutPlus: timerPlus, interval: interval, setArr: setFirstColumnArr},
-    {arr: secondColumnArr, numOfActions: randomNumber(11, 15), timeoutPlus: timerPlus+1, interval:  interval+10, setArr: setSecondColumnArr},
-    {arr: thirdColumnArr, numOfActions: randomNumber(16, 20), timeoutPlus: timerPlus+2, interval:  interval+20, setArr: setThirdColumnArr},
-    {arr: fourthColumnArr,numOfActions: randomNumber(21, 25), timeoutPlus: timerPlus+3, interval:  interval+30, setArr: setFourthColumnArr},
-    {arr: fifthColumnArr, numOfActions: randomNumber(26, 30), timeoutPlus: timerPlus+4, interval:  interval+40, setArr: setFifthColumnArr},
+  const setArr = [ // arr for set arguments to startGame function
+    {arr: firstColumnArr, numOfActions: randomNumber(5, 10), interval: interval, setArr: setFirstColumnArr},
+    {arr: secondColumnArr, numOfActions: randomNumber(11, 15), interval:  interval+10, setArr: setSecondColumnArr},
+    {arr: thirdColumnArr, numOfActions: randomNumber(16, 20), interval:  interval+20, setArr: setThirdColumnArr},
+    {arr: fourthColumnArr,numOfActions: randomNumber(21, 25), interval:  interval+30, setArr: setFourthColumnArr},
+    {arr: fifthColumnArr, numOfActions: randomNumber(26, 30), interval:  interval+40, setArr: setFifthColumnArr},
   ]
 
-  const renderItems = setArr.map((e, index) => <GameColumn items={e.arr} key={index}/>)
+  const renderItems = setArr.map((e, index) => <GameColumn items={e.arr} key={index}/>) // map render items
 
   return (
     <div className="App">
--- src/utils.js
+++ src/utils.js
@@ -1,28 +1,35 @@
-export const gameArr = [
-  {value: 1, content: '1', isActive: true},
-  {value: 2, content: '2', isActive: false},
-  {value: 3, content: '3', isActive: false},
-  {value: 4, content: '4', isActive: false},
-  {value: 5, content: '5', isActive: false},
-  {value: 6, content: '6', isActive: false},
-  {value: 7, content: '7', isActive: false},
-  {value: 8, content: '8', isActive: false},
+export const interval = 150;
+export const timerPlus = 5;
+
+export const gameArr = [ // random arr for game, can contain any number of elements
   {value: 9, content: '9', isActive: false},
+  {value: 8, content: '8', isActive: false},
+  {value: 7, content: '7', isActive: false},
+  {value: 6, content: '6', isActive: false},
+  {value: 5, content: '5', isActive: false},
+  {value: 4, content: '4', isActive: false},
+  {value: 3, content: '3', isActive: false},
+  {value: 2, content: '2', isActive: false},
+  {value: 1, content: '1', isActive: true},
 ];
 
-export const startGame = (arr, func) => {
+export const startGame = (arr, func) => { // 1st arg - one of column arr; 2nd arg - function startGame
   arr.forEach(e => {
-    const { arr, numOfActions, timeoutPlus, interval, setArr } = e;
-    func(arr, numOfActions, timeoutPlus, interval, setArr);
+    const { arr, numOfActions, interval, setArr } = e;
+    func(arr, numOfActions, interval, setArr);
   })
 }
 
-export const columnAction = (arr, numOfActions, timeoutPlus, interval, setArr) => {
+// 1st arg - one of column arr; 2nd - number of actions in a column;
+// 3rd - the change interval of elements in the array; 4th - set the state to the desired column array;
+export const columnAction = (arr, numOfActions, interval, setArr) => { 
   for (let i = 0; i < numOfActions; i++) {
+    // get the integer that remains after dividing by the length of the array
     let u = i % arr.length;
+    // a new array with a shift to one side
     const newArr = [...arr.slice(u, arr.length),  ...arr.slice(0, u)];
-    let timeout = timeoutPlus;
-    let timer = i*interval + timeout*i;
+    let timer = i*interval;
+    console.log('timer', timer);
     
     setTimeout(() => { 
       setArr(newArr)  
@@ -31,5 +38,5 @@ export const columnAction = (arr, numOfActions, timeoutPlus, interval, setArr) =
 }
 
 export const randomNumber = (min, max) => {
-  return Math.floor(Math.random() * (max - min)) + min;
+  return Math.floor(Math.random() * (max - min)) + min; //get random number from min to max
 }
